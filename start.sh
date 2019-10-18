0bac45082de33941bd3d8395666464844b5628a0
Brian Otieno <bryomajor@gmail.com>
2019-10-18 11:57:41 +0300

    Update the keys

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export MOVIE_API_KEY='e7f43a87f01e9efd84f1f174fdc31c6b'
+export MOVIE_API_KEY='f6e919c3947d433554646666b41e65f9'
 export SECRET_KEY='cr3@tey0ur0wnh@ppiness'
 
 python3.6 manage.py server
