07792b08c5e6dc4f1ed24fcb3e0d3898ed2775fd
Brian Otieno <bryomajor@gmail.com>
2019-10-20 22:16:08 +0300

    Register our authentication blueprint instance

--- app/__init__.py
+++ app/__init__.py
@@ -22,9 +22,13 @@ def create_app(config_name):
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    # setting config
+    # Setting config
     from .request import configure_request
     configure_request(app)
+
+    # Register the authentication blueprint
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix = '/authenticate')
 
     # Will add the views and forms
 
